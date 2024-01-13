04c4a45c7f3f71bf67f455f1ce7f207f06161e96
ISeal56 <69968405+Willyema@users.noreply.github.com>
2024-01-14 00:40:06 +0100

    Stop gems from being put in containers

--- src/main/java/me/iseal/powergems/listeners/dropEvent.java
+++ src/main/java/me/iseal/powergems/listeners/dropEvent.java
@@ -2,13 +2,15 @@ package me.iseal.powergems.listeners;
 
 import me.iseal.powergems.Main;
 import me.iseal.powergems.managers.GemManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 
 public class dropEvent implements Listener {
 
@@ -22,4 +24,32 @@ public class dropEvent implements Listener {
         }
     }
 
+    @EventHandler
+    public void onInventoryInteract(InventoryClickEvent e) {
+        if (!(e.getWhoClicked() instanceof Player)) {
+            return;
+        }
+        final Inventory inv = e.getInventory();
+        final ItemStack current = e.getCurrentItem();
+        final ItemStack cursor = e.getCursor();
+        if (inv.getType() != InventoryType.PLAYER) {
+            return;
+        }
+        if (current != null && cursor != null) {
+            if (!current.getType().equals(Material.AIR)) {
+                return;
+            }
+            if (!gm.isGem(cursor)) {
+                e.setCancelled(true);
+            }
+        } else {
+            if (current != null) {
+                return;
+            }
+            if (gm.isGem(cursor)){
+                e.setCancelled(true);
+            }
+        }
+
+    }
 }
